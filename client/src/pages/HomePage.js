3feb3a61b10994b3e2ee380d09acec558ad81f1c
Braden Zingler <bradenzingler@gmail.com>
2024-05-25 16:02:28 -0500

    Transferring frontend over

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import MainHeader from "../components/MainHeader";
+
+function HomePage() {
+  return (
+    <div>
+      <MainHeader />
+      <h1>Join a club today.</h1>
+    </div>
+  );
+}
+
+export default HomePage;
